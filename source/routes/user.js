4e33a64c54ebb537370135e6684c66ffd4b2b347
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-13 21:35:02 -0200

    Create route user/authUser in routes/user.js

--- source/routes/user.js
+++ source/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
-const user = require('../models/user');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -8,10 +9,10 @@ router.post('/createUser', async (req, res) => {
     const { email } = req.body;
 
     try {
-        if(await user.findOne({ email }))
+        if(await User.findOne({ email }))
             return res.status(400).send({ error: 'Usuário já esta cadastrado. (Ref 00x303)'})
 
-        const newUser = await user.create(req.body);
+        const newUser = await User.create(req.body);
 
         newUser.password = undefined;
 
@@ -20,5 +21,21 @@ router.post('/createUser', async (req, res) => {
         return res.status(400).send({ error: 'Ops! Algo inesperado ocorreu ao tentar criar o novo usuário. (Ref 00x302)' + err })
     }
 });
+
+
+router.post('/authUser', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user)
+        return res.status(400).send({ error: 'Hey! Não identificamos o seu email (Ref 00x305)' });
+    
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(403).send({ error: 'Ops! Sua senha esta incorreta (Ref 00x306)' })
+    
+    user.password = undefined;
+    res.send({ user })
+})
 
 module.exports = app => app.use('/user', router);
