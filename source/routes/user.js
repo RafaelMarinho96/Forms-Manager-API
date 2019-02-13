2b4f2b31aff538ff5e674743eba5dfbfc393e6bc
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-13 21:12:42 -0200

    Validate if the email is unique and hides the password after creating the user

--- source/routes/user.js
+++ source/routes/user.js
@@ -5,12 +5,19 @@ const user = require('../models/user');
 const router = express.Router();
 
 router.post('/createUser', async (req, res) => {
+    const { email } = req.body;
+
     try {
+        if(await user.findOne({ email }))
+            return res.status(400).send({ error: 'Usuário já esta cadastrado. (Ref 00x303)'})
+
         const newUser = await user.create(req.body);
+
+        newUser.password = undefined;
 
         return res.send({ newUser })
     } catch (err) {
-        return res.status(400).send({ error: 'Ops! Algo inesperado ocorreu ao tentar criar o novo usuário. (Ref 00x302)' })
+        return res.status(400).send({ error: 'Ops! Algo inesperado ocorreu ao tentar criar o novo usuário. (Ref 00x302)' + err })
     }
 });
 
