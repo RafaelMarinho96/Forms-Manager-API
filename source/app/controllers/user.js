713022a65d8573d9b3fcd407db96e5a2176d6c00
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-23 23:14:19 -0300

    Refactoring the router user/authUser creating controller user

--- source/app/controllers/user.js
+++ source/app/controllers/user.js
@@ -1,4 +1,6 @@
 'use strict'
+const bcrypt = require('bcryptjs');
+
 const userModel = require('../models/user');
 const tokenService = require('../services/tokenService');
 
@@ -24,6 +26,27 @@ async function createUser(req, res){
     }
 }
 
+async function authUser(req, res){
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user)
+        return res.status(400).send({ error: 'Hey! Não identificamos o seu email (Ref 00x305)' });
+    
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(403).send({ error: 'Ops! Sua senha esta incorreta (Ref 00x306)' })
+    
+    user.password = undefined;
+    res.send(
+        { 
+            user,
+            token: tokenService.generateToken({ id: user.id })
+        }
+    )
+}
+
 module.exports = {
-    createUser
+    createUser,
+    authUser
 }
--- source/app/routes/user.js
+++ source/app/routes/user.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-const User = require('../models/user');
-const authConfig = require('../../config/auth');
-const tokenService = require('../services/tokenService');
 const userController = require('../controllers/user');
 
 const router = express.Router();
 
 router.post('/createUser', userController.createUser);
-
-router.post('/authUser', async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email }).select('+password');
-
-    if (!user)
-        return res.status(400).send({ error: 'Hey! Não identificamos o seu email (Ref 00x305)' });
-    
-    if(!await bcrypt.compare(password, user.password))
-        return res.status(403).send({ error: 'Ops! Sua senha esta incorreta (Ref 00x306)' })
-    
-    user.password = undefined;
-    res.send(
-        { 
-            user,
-            token: tokenService.generateToken({ id: user.id })
-        }
-    )
-})
+router.post('/authUser', userController.authUser);
 
 module.exports = app => app.use('/user', router);
