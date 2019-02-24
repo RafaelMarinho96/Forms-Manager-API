cd00f3ba66127d1bec8b3040ac94007bb0fc23e1
RafaelMarinho96 <rafael.marinho07@gmail.com>
2019-02-23 22:20:38 -0300

    Installed libs FS and PATH to load routes

--- source/app.js
+++ source/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-require('./app/routes/group')(app);
-require('./app/routes/user')(app);
+require('./app/routes/index')(app);
+
 
 module.exports = app;
